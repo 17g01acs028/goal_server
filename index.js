646dfebb5444b293b64deb391a9d437ca8b2c644
17g01acs028 <stevemutioo123@gmail.com>
2024-04-10 13:44:24 +0300

    latest update

--- index.js
+++ index.js
@@ -3,12 +3,8 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
-import path from "path";
-import { fileURLToPath } from "url";
-
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import weekRoutes from "./routes/week.js";
@@ -18,12 +14,7 @@ import categoryRoutes from "./routes/category.js"
 
 import { register } from "./controllers/auth.js";
 
-//import User from "./models/User.js";
-//import { users} from "./data/index.js";
 
-// /* CONFIGURATIONS */
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -33,18 +24,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-// app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-// /* FILE STORAGE */
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "public/assets");
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, file.originalname);
-//   },
-// });
-// const upload = multer({ storage });
 
 // /* ROUTES WITH FILES */
 app.post("/auth/register", register);
